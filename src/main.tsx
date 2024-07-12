535e4727fa0c20e03c9c3bec6ee32031e41ac803
Dendosha <thenocho22@gmail.com>
2024-07-12 18:22:33 +0500

    add store provider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.scss';
+import { store } from './store/store.ts';
 
 const router = createBrowserRouter([
 	{
@@ -12,6 +14,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
 	</React.StrictMode>
 );
