43ee661c14d6cd790f75744d3f826a8695ee70eb
Dendosha <thenocho22@gmail.com>
2024-07-14 18:32:00 +0500

    update store using redux-persist

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,45 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+	FLUSH,
+	PAUSE,
+	PERSIST,
+	persistReducer,
+	persistStore,
+	PURGE,
+	REGISTER,
+	REHYDRATE
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import foldersSlice from './folders.slice';
 import notesSlice from './notes.slice';
 import settingsSlice from './settings.slice';
 import tasksSlice from './tasks.slice';
 
-export const store = configureStore({
-	reducer: {
-		notes: notesSlice,
-		tasks: tasksSlice,
-		folders: foldersSlice,
-		settings: settingsSlice
-	}
+const rootStore = combineReducers({
+	notes: notesSlice,
+	tasks: tasksSlice,
+	folders: foldersSlice,
+	settings: settingsSlice
 });
+
+const persistConfig = {
+	key: 'root',
+	storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootStore);
+
+export const store = configureStore({
+	reducer: persistedReducer,
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+			}
+		})
+});
+
+export const persistor = persistStore(store);
 
 export type RootStore = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
