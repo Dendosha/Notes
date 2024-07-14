152cb2867e1a75a9d14da4092ae6859fb86fbf31
Dendosha <thenocho22@gmail.com>
2024-07-14 22:14:34 +0500

    update CustomModal component

--- src/components/Modals/CustomModal/CustomModal.props.ts
+++ src/components/Modals/CustomModal/CustomModal.props.ts
@@ -3,7 +3,5 @@ import { ModalOwnProps } from '@mui/base';
 export interface CustomModalProps
 	extends Omit<ModalOwnProps, 'slots' | 'slotProps'> {
 	className?: string | undefined;
-	handleClose: () => void;
 	'aria-labelledby': string;
-	'aria-describedby': string;
 }
