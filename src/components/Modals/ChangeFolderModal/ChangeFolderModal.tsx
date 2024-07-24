7237d1b6e4f5d74f1136aec2fa80d72c124a9f70
Dendosha <thenocho22@gmail.com>
2024-07-24 20:16:28 +0500

    create ChangeFolderModal component

---
+++ src/components/Modals/ChangeFolderModal/ChangeFolderModal.tsx
@@ -0,0 +1,101 @@
+import { FormEvent, useState } from 'react';
+import { useAppDispatch } from '../../../hooks/useAppDispatch.hook';
+import { useAppSelector } from '../../../hooks/useAppSelector.hook';
+import { foldersActions } from '../../../store/folders.slice';
+import { notesActions } from '../../../store/notes.slice';
+import CustomSelect from '../../CustomSelect/CustomSelect';
+import { CustomSelectItem } from '../../CustomSelect/CustomSelect.props';
+import TextButton from '../../TextButton/TextButton';
+import CustomModal from '../CustomModal/CustomModal';
+import styles from './ChangeFolderModal.module.scss';
+import { ChangeFolderModalProps } from './ChangeFolderModal.props';
+
+const CHANGE_FOLDER_MODAL_TITLE = 'change-folder-modal-title';
+
+function ChangeFolderModal({
+	notes,
+	modalState,
+	setModalState
+}: ChangeFolderModalProps) {
+	const dispatch = useAppDispatch();
+	const folders = useAppSelector(state => state.folders);
+
+	const [newFolder, setNewFodler] = useState<string | number>('');
+
+	const customSelectItems = folders.items.map(
+		(item): CustomSelectItem<number> => ({
+			value: item.id,
+			label: item.name
+		})
+	);
+
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		const formData = new FormData(e.currentTarget);
+		const folderId = parseInt(formData.get('folder') as string);
+
+		dispatch(
+			foldersActions.updateNotes({
+				id: folderId,
+				notes: notes
+			})
+		);
+
+		notes.forEach(item => {
+			dispatch(
+				notesActions.update({
+					...item,
+					folderId: folderId
+				})
+			);
+		});
+
+		setModalState(false);
+	};
+
+	const closeModal = () => {
+		setModalState(false);
+	};
+
+	return (
+		<CustomModal
+			open={modalState}
+			onClose={closeModal}
+			aria-labelledby={CHANGE_FOLDER_MODAL_TITLE}
+		>
+			<div>
+				<form onSubmit={handleSubmit} className={styles['modal-form']}>
+					<h2 id={CHANGE_FOLDER_MODAL_TITLE} className='visually-hidden'>
+						Переместить заметки в другую папку
+					</h2>
+					<CustomSelect
+						name='folder'
+						setValue={setNewFodler}
+						items={customSelectItems}
+						defaultValue={''}
+						placeholder={'Выберите папку'}
+					/>
+					<div className={styles['modal-buttons']}>
+						<TextButton
+							type='button'
+							onClick={closeModal}
+							className={styles['modal-buttons__button']}
+						>
+							Отмена
+						</TextButton>
+						<TextButton
+							type='submit'
+							className={styles['modal-buttons__button']}
+							disabled={!newFolder}
+						>
+							Переместить
+						</TextButton>
+					</div>
+				</form>
+			</div>
+		</CustomModal>
+	);
+}
+
+export default ChangeFolderModal;
