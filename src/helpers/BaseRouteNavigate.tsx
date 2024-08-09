6c9ab9ae8d6f973501d1786be074de85a32554dc
Dendosha <thenocho22@gmail.com>
2024-08-09 21:47:26 +0500

    update BaseRouteNavigate helper
    add base note folder creation

--- src/helpers/BaseRouteNavigate.tsx
+++ src/helpers/BaseRouteNavigate.tsx
@@ -1,11 +1,29 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
+import { useAppDispatch } from '../hooks/useAppDispatch.hook';
+import { useAppSelector } from '../hooks/useAppSelector.hook';
+import { foldersActions } from '../store/folders.slice';
 
 function BaseRouteNavigate({ children }: { children: ReactNode }) {
+	const dispatch = useAppDispatch();
+	const folders = useAppSelector(state => state.folders);
+
 	const location = useLocation();
 
-	if (location.pathname === '/') {
-		return <Navigate to={'/notes'} replace={true} />;
+	useEffect(() => {
+		if (!folders.items.find(folder => folder.id === 1)) {
+			dispatch(
+				foldersActions.add({
+					id: 1,
+					name: 'Все',
+					notes: []
+				})
+			);
+		}
+	}, []);
+
+	if (location.pathname === '/' || location.pathname === '/notes') {
+		return <Navigate to={'/notes/all'} replace={true} />;
 	}
 
 	return children;
