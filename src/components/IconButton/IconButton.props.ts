486c3774578282a87c07bd584ca2139d0112a010
Dendosha <thenocho22@gmail.com>
2024-08-12 00:24:49 +0500

    update IconButton component
    add forwardRed; add colorScheme prop; change styles

--- src/components/IconButton/IconButton.props.ts
+++ src/components/IconButton/IconButton.props.ts
@@ -3,5 +3,6 @@ import { ButtonHTMLAttributes } from 'react';
 export interface IconButtonProps
 	extends ButtonHTMLAttributes<HTMLButtonElement> {
 	appearance?: 'circle' | 'polygon';
+	colorScheme?: 'default' | 'primary';
 	iconType?: 'fill' | 'stroke' | 'both';
 }
--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -1,28 +1,38 @@
 import cn from 'classnames';
+import { forwardRef } from 'react';
 import styles from './IconButton.module.scss';
 import { IconButtonProps } from './IconButton.props';
 
-function IconButton({
-	appearance = 'polygon',
-	iconType = 'fill',
-	children,
-	className,
-	...props
-}: IconButtonProps) {
-	return (
-		<button
-			className={cn(styles['icon-button'], className, {
-				[styles['icon-button_polygon']]: appearance === 'polygon',
-				[styles['icon-button_circle']]: appearance === 'circle',
-				[styles['icon-button_fill']]: iconType === 'fill',
-				[styles['icon-button_stroke']]: iconType === 'stroke',
-				[styles['icon-button_fill-and-stroke']]: iconType === 'both'
-			})}
-			{...props}
-		>
-			{children}
-		</button>
-	);
-}
+const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
+	function IconButton(
+		{
+			appearance = 'polygon',
+			colorScheme = 'default',
+			iconType = 'fill',
+			children,
+			className,
+			...props
+		},
+		ref
+	) {
+		return (
+			<button
+				className={cn(styles['icon-button'], className, {
+					[styles['icon-button_primary-color-scheme']]:
+						colorScheme === 'primary',
+					[styles['icon-button_polygon']]: appearance === 'polygon',
+					[styles['icon-button_circle']]: appearance === 'circle',
+					[styles['icon-button_fill']]: iconType === 'fill',
+					[styles['icon-button_stroke']]: iconType === 'stroke',
+					[styles['icon-button_fill-and-stroke']]: iconType === 'both'
+				})}
+				ref={ref}
+				{...props}
+			>
+				{children}
+			</button>
+		);
+	}
+);
 
 export default IconButton;
