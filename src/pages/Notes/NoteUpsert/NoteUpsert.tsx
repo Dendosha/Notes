c83c7493511f930513e28e2949218cbefdf4333a
Dendosha <thenocho22@gmail.com>
2024-08-12 01:52:49 +0500

    create NotesUpsert page

---
+++ src/pages/Notes/NoteUpsert/NoteUpsert.tsx
@@ -0,0 +1,134 @@
+import { useEffect, useRef } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import BackButtonIcon from '../../../assets/icons/BackButtonIcon';
+import IconButton from '../../../components/IconButton/IconButton';
+import TextField from '../../../components/TextField/TextField';
+import { useAppDispatch } from '../../../hooks/useAppDispatch.hook';
+import { useAppSelector } from '../../../hooks/useAppSelector.hook';
+import { foldersActions } from '../../../store/folders.slice';
+import { notesActions } from '../../../store/notes.slice';
+import styles from './NoteUpsert.module.scss';
+
+function NoteUpsert() {
+	const dispatch = useAppDispatch();
+	const notes = useAppSelector(state => state.notes);
+
+	const navigate = useNavigate();
+	const { folder, note } = useParams();
+	const folderId = parseInt(folder?.split('-')[1] ?? '1');
+	const noteId = parseInt(note?.split('-')[1]!);
+
+	const noteExist = notes.items.find(item => item.id === noteId);
+
+	const buttonRef = useRef<HTMLButtonElement>(null);
+	const titleRef = useRef<HTMLInputElement>(null);
+	const descriptionRef = useRef<HTMLTextAreaElement>(null);
+
+	useEffect(() => {
+		const createButton = document.activeElement as HTMLButtonElement | null;
+
+		titleRef.current?.focus();
+
+		function handleEscape(e: KeyboardEvent) {
+			if (e.key === 'Escape') {
+				handleExit();
+			}
+		}
+
+		document.addEventListener('keydown', handleEscape);
+
+		return () => {
+			createButton?.focus();
+			document.removeEventListener('keydown', handleEscape);
+		};
+	}, []);
+
+	const createNote = () => {
+		dispatch(
+			notesActions.add({
+				id: noteId,
+				title: titleRef.current?.value ?? '',
+				content: descriptionRef.current?.value ?? '',
+				folderId: folderId !== 1 ? folderId : undefined
+			})
+		);
+
+		dispatch(
+			foldersActions.updateNotes({
+				id: folderId,
+				notes: [noteId]
+			})
+		);
+	};
+
+	const updateNote = () => {
+		dispatch(
+			notesActions.update({
+				id: noteId,
+				title: titleRef.current?.value!,
+				content: descriptionRef.current?.value!
+			})
+		);
+	};
+
+	const handleExit = () => {
+		navigate(-1);
+
+		if (!titleRef.current?.value && !descriptionRef.current?.value) {
+			return;
+		}
+
+		if (noteExist) {
+			updateNote();
+		} else {
+			createNote();
+		}
+	};
+
+	const handleKeyDown = (e: React.KeyboardEvent) => {
+		if (e.key !== 'Tab') {
+			return;
+		}
+
+		if (e.shiftKey && document.activeElement === buttonRef.current) {
+			e.preventDefault();
+			descriptionRef.current?.focus();
+		} else if (
+			!e.shiftKey &&
+			document.activeElement === descriptionRef.current
+		) {
+			e.preventDefault();
+			buttonRef.current?.focus();
+		}
+	};
+
+	return (
+		<div className={styles['note-edit']} onKeyDown={handleKeyDown}>
+			<div className={styles['note-edit__header']}>
+				<IconButton iconType='stroke' onClick={handleExit} ref={buttonRef}>
+					<BackButtonIcon />
+				</IconButton>
+				<TextField
+					as='input'
+					ref={titleRef}
+					autoComplete='off'
+					name='note-title'
+					defaultValue={noteExist?.title}
+					placeholder='Заголовок'
+				/>
+			</div>
+			<div className={styles['note-edit__content']}>
+				<TextField
+					as='textarea'
+					ref={descriptionRef}
+					name='note-description'
+					defaultValue={noteExist?.content}
+					placeholder='Описание'
+					className={styles['note-edit__description']}
+				/>
+			</div>
+		</div>
+	);
+}
+
+export default NoteUpsert;
