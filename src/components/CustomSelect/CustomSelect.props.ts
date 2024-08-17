9e19f50ed874bcbeb61c54c6dd0316eaa6ded138
Dendosha <thenocho22@gmail.com>
2024-08-17 23:12:41 +0500

    update CustomSelect component

--- src/components/CustomSelect/CustomSelect.props.ts
+++ src/components/CustomSelect/CustomSelect.props.ts
@@ -2,16 +2,15 @@ import { SelectOwnProps } from '@mui/base';
 
 export type OptionValueType = string | number;
 
-export interface CustomSelectItem<T extends OptionValueType> {
+export interface CustomSelectItem<T> {
 	value: T;
 	label: string;
 }
 
-export interface CustomSelectOwnProps<T extends OptionValueType> {
+export interface CustomSelectOwnProps<T> {
 	items: CustomSelectItem<T>[];
 	setValue: (state: React.SetStateAction<T>) => void;
 }
 
-export type CustomSelectProps<T extends OptionValueType> =
-	CustomSelectOwnProps<T> &
-		Omit<SelectOwnProps<T, boolean>, 'slots' | 'slotProps'>;
+export type CustomSelectProps<T> = CustomSelectOwnProps<T> &
+	Omit<SelectOwnProps<{}, boolean>, 'slots' | 'slotProps'>;
--- src/components/CustomSelect/CustomSelect.tsx
+++ src/components/CustomSelect/CustomSelect.tsx
@@ -11,44 +11,43 @@ import ArrowIcon from '../../assets/icons/ArrowIcon';
 import styles from './CustomSelect.module.scss';
 import { CustomSelectProps } from './CustomSelect.props';
 
-const CustomSelect = forwardRef<
-	HTMLButtonElement,
-	CustomSelectProps<string | number>
->(function CustomSelect(
-	{ items, defaultValue = items[0].value, setValue, className, ...props },
-	ref
-) {
-	return (
-		<div className={cn(styles['custom-select-wrapper'], className)}>
-			<Select
-				ref={ref}
-				className={styles['custom-select']}
-				slots={{ root: Button, listbox: AnimatedListbox }}
-				slotProps={{
-					root: { className: styles['custom-select__button'] },
-					popup: {
-						className: styles['custom-select__popup'],
-						disablePortal: true
-					},
-					listbox: { className: styles['custom-select__listbox'] }
-				}}
-				defaultValue={defaultValue}
-				{...props}
-			>
-				{items.map(item => (
-					<Option
-						key={item.value}
-						value={item.value}
-						onClick={() => setValue(item.value)}
-						className={styles['custom-select__option']}
-					>
-						{item.label}
-					</Option>
-				))}
-			</Select>
-		</div>
-	);
-});
+const CustomSelect = forwardRef<HTMLButtonElement, CustomSelectProps<any>>(
+	function CustomSelect(
+		{ items, defaultValue = items[0].value, setValue, className, ...props },
+		ref
+	) {
+		return (
+			<div className={cn(styles['custom-select-wrapper'], className)}>
+				<Select
+					ref={ref}
+					className={styles['custom-select']}
+					slots={{ root: Button, listbox: AnimatedListbox }}
+					slotProps={{
+						root: { className: styles['custom-select__button'] },
+						popup: {
+							className: styles['custom-select__popup'],
+							disablePortal: true
+						},
+						listbox: { className: styles['custom-select__listbox'] }
+					}}
+					defaultValue={defaultValue}
+					{...props}
+				>
+					{items.map(item => (
+						<Option
+							key={item.value}
+							value={item.value}
+							onClick={() => setValue(item.value)}
+							className={styles['custom-select__option']}
+						>
+							{item.label}
+						</Option>
+					))}
+				</Select>
+			</div>
+		);
+	}
+);
 
 const Button = forwardRef(function Button<
 	TValue extends object,
