144be21b5e208046b54dbd1f374e96723cd0db7b
Dendosha <thenocho22@gmail.com>
2024-09-01 01:06:33 +0500

    add Loading component

---
+++ src/pages/Loading/Loading.tsx
@@ -0,0 +1,18 @@
+function Loading() {
+	return (
+		<div
+			style={{
+				position: 'absolute',
+				inset: '0',
+				display: 'grid',
+				justifyContent: 'center',
+				alignItems: 'center',
+				fontSize: '40px'
+			}}
+		>
+			Загрузка...
+		</div>
+	);
+}
+
+export default Loading;
